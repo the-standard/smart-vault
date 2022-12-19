53481392b3d2ad20eaa97272e3a0c7e25ec67fd3
Ewan Sheldon <ewansheldon@gmail.com>
2022-12-19 16:49:14 +0000

    add eth collateral

--- contracts/SmartVault.sol
+++ contracts/SmartVault.sol
@@ -4,4 +4,8 @@ pragma solidity 0.8.17;
 contract SmartVault {
     uint256 public collateral;
     uint256 public minted;
+
+    function addCollateralETH() external payable {
+        collateral = msg.value;
+    }
 }
--- test/SmartVault.js
+++ test/SmartVault.js
@@ -1,12 +1,25 @@
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
+let vault;
+
 describe('SmartVault', async () => {
+  beforeEach(async () => {
+    vault = await (await ethers.getContractFactory('SmartVault')).deploy();
+  });
+
   describe('opening', async () => {
     it('opens a vault with no collateral deposited, and no tokens minted', async () => {
-      const vault = await (await ethers.getContractFactory('SmartVault')).deploy();
       expect(await vault.collateral()).to.equal(0);
       expect(await vault.minted()).to.equal(0);
+    });
+  });
+
+  describe('addCollateralETH', async () => {
+    it('accepts ETH as collateral', async () => {
+      const value = ethers.utils.parseEther('1');
+      await vault.addCollateralETH({value: value});
+      expect(await vault.collateral()).to.equal(value);
     });
   });
 });
