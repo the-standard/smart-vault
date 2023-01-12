4515c69ce619ae45363b0e1b41cb660b27932e11
Ewan Sheldon <ewansheldon@gmail.com>
2023-01-12 16:12:29 +0100

    add goerli network to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('hardhat-contract-sizer');
+require('dotenv').config();
+
+const { INFURA_API_KEY, TEST_ACCOUNT_PRIVATE_KEY } = process.env;
+const testAccounts = TEST_ACCOUNT_PRIVATE_KEY ? [TEST_ACCOUNT_PRIVATE_KEY] : [];
 
 module.exports = {
   solidity: "0.8.17",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 20
-    },
+  defaultNetwork: 'hardhat',
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: testAccounts
+    }
   },
 };
