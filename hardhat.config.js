d08743fb80a68fc36b917805d801b7f84232b2ca
Ewan Sheldon <ewansheldon@gmail.com>
2023-03-17 10:26:09 +0100

    add etherscan verification to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 require('hardhat-contract-sizer');
 require('solidity-coverage')
+require("@nomiclabs/hardhat-etherscan");
 
-const { INFURA_API_KEY, TEST_ACCOUNT_PRIVATE_KEY } = process.env;
+const { INFURA_API_KEY, TEST_ACCOUNT_PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
 const testAccounts = TEST_ACCOUNT_PRIVATE_KEY ? [TEST_ACCOUNT_PRIVATE_KEY] : [];
 
 module.exports = {
@@ -14,9 +15,16 @@ module.exports = {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
       accounts: testAccounts
     },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: testAccounts
+    },
     polygon_zk_testnet: {
       url: 'https://rpc.public.zkevm-test.net',
       accounts: testAccounts
     }
   },
+  etherscan: {
+    apiKey: ETHERSCAN_KEY,
+  }
 };
