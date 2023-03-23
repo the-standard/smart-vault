6e4a6a056124721a3ba93c56dd24296687025ca7
Ewan Sheldon <ewansheldon@gmail.com>
2023-03-23 15:02:29 +0100

    update default test prices

--- test/common.js
+++ test/common.js
@@ -3,8 +3,8 @@ const { BigNumber } = ethers;
 
 const HUNDRED_PC = BigNumber.from(100000);
 const DEFAULT_COLLATERAL_RATE = BigNumber.from(120000); // 120%
-const DEFAULT_ETH_USD_PRICE = BigNumber.from(125000000000); // $1250
-const DEFAULT_EUR_USD_PRICE = BigNumber.from(105000000); // $1.05
+const DEFAULT_ETH_USD_PRICE = BigNumber.from(160000000000); // $1600
+const DEFAULT_EUR_USD_PRICE = BigNumber.from(106000000); // $1.06
 const PROTOCOL_FEE_RATE = BigNumber.from(1000); // 1%
 const ETH = ethers.utils.formatBytes32String('ETH');
 
