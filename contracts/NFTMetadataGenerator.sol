4ffe7ba2b7b0d54b9f7dd0bd1615314565db9fe2
Ewan Sheldon <ewansheldon@gmail.com>
2023-03-31 14:55:09 +0200

    add nft generator dependency to smart vault manager

---
+++ contracts/NFTMetadataGenerator.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "contracts/interfaces/ISmartVault.sol";
+import "contracts/interfaces/INFTMetadataGenerator.sol";
+
+contract NFTMetadataGenerator is INFTMetadataGenerator {
+    using Strings for uint256;
+
+    function mapCollateral(ISmartVault.Asset[] memory _collateral) private pure returns (string memory collateralTraits) {
+        collateralTraits = "";
+        for (uint256 i = 0; i < _collateral.length; i++) {
+            ISmartVault.Asset memory asset = _collateral[i];
+            collateralTraits = string(abi.encodePacked(collateralTraits, '{"trait_type":"', "ETH", '", ','"display_type": "boost_number",','"value": ',(asset.amount / 1 ether).toString(),'},'));
+        }
+    }
+
+    function generateSvg(uint256 _tokenId, ISmartVault.Status memory _vaultStatus) private pure returns (string memory) {
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+                "<style>.header { font-weight: 700; } .base { fill: #6cf; font-family: Arial, Helvetica, sans-serif; font-size: 12px; }</style>",
+                '<rect width="100%" height="100%" fill="rgb(40,40,40)" />',
+                '<text x="50%" y="20%" class="header base" dominant-baseline="middle" text-anchor="middle">',
+                    "The Standard Smart Vault #",_tokenId.toString(),
+                "</text>",
+                '<text x="50%" y="30%" class="base" dominant-baseline="middle" text-anchor="middle">',
+                    "Version: ", uint256(_vaultStatus.version).toString(),
+                "</text>",
+                '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
+                    "Borrowed: ", (_vaultStatus.minted / 1 ether).toString(), " sEURO",
+                "</text>",
+                '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+                    "Current Borrow Limit: ", (_vaultStatus.maxMintable / 1 ether).toString(), " sEURO",
+                "</text>",
+                '<text x="50%" y="60%" class="base" dominant-baseline="middle" text-anchor="middle">',
+                    "Collateral %: ", (_vaultStatus.currentCollateralPercentage / 1000).toString(), "%",
+                "</text>",
+            "</svg>"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(svg)
+                )
+            );
+    }
+
+    function generateNFTMetadata(uint256 _tokenId, ISmartVault.Status memory _vaultStatus) external pure returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+                '"name": "The Standard Smart Vault #',_tokenId.toString(),'",',
+                '"description": "The Standard Smart Vault (',"seuro",')",',
+                '"attributes": [',
+                    '{"trait_type": "Status", "value": "',_vaultStatus.liquidated ?"liquidated":"active",'"},',
+                    '{"trait_type": "Borrowed", "value": ', (_vaultStatus.minted / 1 ether).toString(),'},',
+                    '{"trait_type": "Max Borrowable Amount", "display_type": "number", "value": "',(_vaultStatus.maxMintable / 1 ether).toString(),'"},',
+                    '{"trait_type": "Collateral %", "display_type": "boost_percentage", "value": ',(_vaultStatus.currentCollateralPercentage / 1000).toString(),'},',
+                    mapCollateral(_vaultStatus.collateral),
+                    '{"trait_type": "Version", "value": "',uint256(_vaultStatus.version).toString(),'"},',
+                    '{"trait_type": "Vault Type", "value": "',"seuro",'"}',
+                '],',
+                '"image": "',generateSvg(_tokenId, _vaultStatus),'"',
+            "}"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(dataURI)
+                )
+            );
+    }
+}
