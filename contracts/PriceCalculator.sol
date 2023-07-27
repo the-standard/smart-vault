9b33c537d2dee8e58e4cdf699c49ed5211c5dbad
Ewan Sheldon <ewansheldon@gmail.com>
2023-07-27 10:31:56 +0200

    allow for missing data feed rounds

--- contracts/PriceCalculator.sol
+++ contracts/PriceCalculator.sol
@@ -30,7 +30,7 @@ contract PriceCalculator is IPriceCalculator {
                 accummulatedRoundPrices += uint256(answer);
                 roundCount++;
             } catch {
-                // do nothing, continue loop
+                // do nothing
             }
         }
         return accummulatedRoundPrices / roundCount;
