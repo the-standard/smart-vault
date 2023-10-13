e335cb1a239ec43624a9fd5f9943a73f882cd7c5
Ewan Sheldon <ewansheldon@gmail.com>
2023-10-13 18:04:55 +0200

    upgrade and verification scripts

--- scripts/upgradeSmartVaultManager.js
+++ scripts/upgradeSmartVaultManager.js
@@ -1,23 +1,32 @@
 const { ethers, upgrades } = require("hardhat");
-const { PROTOCOL_FEE_RATE } = require("../test/common");
+const { PROTOCOL_FEE_RATE, DEFAULT_EUR_USD_PRICE } = require("../test/common");
 
 async function main() {
-  const managerAddress = '0x2342755a637451e9af75545e257Cb007EaC930B1';
+  const managerAddress = '0xba169cceCCF7aC51dA223e04654Cf16ef41A68CC';
+  const deployerV2 = await (await ethers.getContractFactory('SmartVaultDeployerV2')).deploy(ethers.utils.formatBytes32String('ETH'), '0xa14d53bc1f1c0f31b4aa3bd109344e5009051a84');
+  await deployerV2.deployed();
   const v2 = await upgrades.upgradeProxy(managerAddress,
     await ethers.getContractFactory('SmartVaultManagerV2'));
 
   const fee = await v2.setSwapFeeRate(PROTOCOL_FEE_RATE);
   await fee.wait();
-  const weth = await v2.setWethAddress('0xe39Ab88f8A4777030A534146A9Ca3B52bd5D43A3');
+  const weth = await v2.setWethAddress('0x82aF49447D8a07e3bd95BD0d56f35241523fBab1');
   await weth.wait();
   const router = await v2.setSwapRouterAddress('0xE592427A0AEce92De3Edee1F18E0157C05861564');
   await router.wait();
+  const setDeployer = await v2.setSmartVaultDeployer(deployerV2.address);
+  await setDeployer.wait();
 
   await new Promise(resolve => setTimeout(resolve, 60000));
 
   await run(`verify:verify`, {
     address: managerAddress,
     constructorArguments: [],
+  });
+
+  await run(`verify:verify`, {
+    address: deployerV2.address,
+    constructorArguments: [ethers.utils.formatBytes32String('ETH'), '0xa14d53bc1f1c0f31b4aa3bd109344e5009051a84']
   });
 }
 
--- scripts/verifyVault.js
+++ scripts/verifyVault.js
@@ -3,19 +3,20 @@ const { ethers, upgrades } = require("hardhat");
 async function main() {
   const [ user ] = await ethers.getSigners();
 
-  const vault = '0x34B2b38F5ac25cB5Bcb4c19150f01baDCFb02cCb';
-  const native = ethers.utils.formatBytes32String('AGOR');
-  const manager = '0x6A301a76f67ECf0D56377F8Db384dbCa9E161203';
+  const vault = '0x2579E077826BC309B2FDD02bAEc87508d91Ff86F';
+  const native = ethers.utils.formatBytes32String('ETH');
+  const manager = await ethers.getContractAt('SmartVaultManagerV2', '0xba169cceCCF7aC51dA223e04654Cf16ef41A68CC')
   const owner = user.address;
-  const euros = '0x9C777AD2575010E3ED67F6E849cfE1115BFE2A50'
-  const calculator = '0xA35Ff34200432F573e0Db493872cDbc18e2d5E08'
+  const euros = '0x643b34980E635719C15a2D4ce69571a258F940E9'
+  const calculator = '0x97517b3Fef774cBe3f520253cDf04067A4b9aaFb'
 
-  await run(`verify:verify`, {
-    address: vault,
-    constructorArguments: [
-      native, manager, owner, euros, calculator
-    ],
-  });
+  // await run(`verify:verify`, {
+  //   address: vault,
+  //   constructorArguments: [
+  //     native, manager.address, owner, euros, calculator
+  //   ],
+  // });
+  console.log(await manager.connect(user).vaults());
 }
 
 main().catch((error) => {
