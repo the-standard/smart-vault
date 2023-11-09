e925090ea6394ed15429fe95605ff3372693283f
Ewan Sheldon <ewansheldon@gmail.com>
2023-11-09 18:03:18 +0100

    update upgrade script

--- scripts/upgradeSmartVaultManager.js
+++ scripts/upgradeSmartVaultManager.js
@@ -3,30 +3,14 @@ const { PROTOCOL_FEE_RATE, DEFAULT_EUR_USD_PRICE } = require("../test/common");
 
 async function main() {
   const managerAddress = '0xba169cceCCF7aC51dA223e04654Cf16ef41A68CC';
-  const deployerV2 = await (await ethers.getContractFactory('SmartVaultDeployerV2')).deploy(ethers.utils.formatBytes32String('ETH'), '0xa14d53bc1f1c0f31b4aa3bd109344e5009051a84');
-  await deployerV2.deployed();
-  const v2 = await upgrades.upgradeProxy(managerAddress,
-    await ethers.getContractFactory('SmartVaultManagerV2'));
-
-  const fee = await v2.setSwapFeeRate(PROTOCOL_FEE_RATE);
-  await fee.wait();
-  const weth = await v2.setWethAddress('0x82aF49447D8a07e3bd95BD0d56f35241523fBab1');
-  await weth.wait();
-  const router = await v2.setSwapRouterAddress('0xE592427A0AEce92De3Edee1F18E0157C05861564');
-  await router.wait();
-  const setDeployer = await v2.setSmartVaultDeployer(deployerV2.address);
-  await setDeployer.wait();
+  const v3 = await upgrades.upgradeProxy(managerAddress,
+    await ethers.getContractFactory('SmartVaultManagerV3'));
 
   await new Promise(resolve => setTimeout(resolve, 60000));
-
+  
   await run(`verify:verify`, {
-    address: managerAddress,
+    address: v3.address,
     constructorArguments: [],
-  });
-
-  await run(`verify:verify`, {
-    address: deployerV2.address,
-    constructorArguments: [ethers.utils.formatBytes32String('ETH'), '0xa14d53bc1f1c0f31b4aa3bd109344e5009051a84']
   });
 }
 
