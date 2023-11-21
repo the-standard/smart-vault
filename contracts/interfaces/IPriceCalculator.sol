696be301fcf354745a27cdbe9340d35f288ff537
Ewan Sheldon <ewansheldon@gmail.com>
2023-11-21 10:36:06 +0100

    new price calc inteface

--- contracts/interfaces/IPriceCalculator.sol
+++ contracts/interfaces/IPriceCalculator.sol
@@ -4,5 +4,7 @@ pragma solidity 0.8.17;
 import "contracts/interfaces/ITokenManager.sol";
 
 interface IPriceCalculator {
+    function tokenToEurAvg(ITokenManager.Token memory _token, uint256 _amount) external view returns (uint256);
     function tokenToEur(ITokenManager.Token memory _token, uint256 _amount) external view returns (uint256);
+    function eurToToken(ITokenManager.Token memory _token, uint256 _amount) external view returns (uint256);
 }
