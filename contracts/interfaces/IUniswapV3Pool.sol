85c09486b823fe39bb42ada1a7979e20d4c897e3
Ewan Sheldon <ewansheldon@gmail.com>
2024-11-22 15:33:27 +0100

    add ticks + liquidity intefaces to uni pool int

--- contracts/interfaces/IUniswapV3Pool.sol
+++ contracts/interfaces/IUniswapV3Pool.sol
@@ -24,4 +24,17 @@ interface IUniswapV3Pool {
     function tickSpacing() external view returns (int24);
 
     function increaseObservationCardinalityNext(uint16 observationCardinalityNext) external;
+
+    function liquidity() external returns (uint128);
+    
+    function ticks(int24 tick) external view returns (
+        uint128 liquidityGross,
+        int128 liquidityNet,
+        uint256 feeGrowthOutside0X128,
+        uint256 feeGrowthOutside1X128,
+        int56 tickCumulativeOutside,
+        uint160 secondsPerLiquidityOutsideX128,
+        uint32 secondsOutside,
+        bool initialized
+    );
 }
