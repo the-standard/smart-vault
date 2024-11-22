1e9bb13063c793e220b9e7f773130f126d374b9a
Ewan Sheldon <ewansheldon@gmail.com>
2024-11-22 18:11:44 +0100

    withdraw if hypervisor address given

--- contracts/interfaces/ISmartVaultManagerV3.sol
+++ contracts/interfaces/ISmartVaultManagerV3.sol
@@ -6,4 +6,5 @@ import "contracts/interfaces/ISmartVaultManagerV2.sol";
 
 interface ISmartVaultManagerV3 is ISmartVaultManagerV2, ISmartVaultManager {
     function yieldManager() external view returns (address);
+    function vaultAutoRedemption(uint256 _tokenID, address _collateralAddr, bytes memory _swapPath, uint256 _collateralAmount, address _hypervisor) external  returns (uint256 _amountOut);
 }
