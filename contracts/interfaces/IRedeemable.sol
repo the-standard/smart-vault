417f41ffec412171ca352142ff41f8917382cdff
Ewan Sheldon <ewansheldon@gmail.com>
2024-11-28 12:42:22 +0100

    vault interface for legacy + up to date vaults

---
+++ contracts/interfaces/IRedeemable.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+interface IRedeemable {
+    function autoRedemption(
+        address _swapRouterAddress,
+        address _quoterAddress,
+        address _collateralToken,
+        bytes memory _swapPath,
+        uint256 _USDCTargetAmount,
+        address _hypervisor
+    ) external;
+}
+
+interface IRedeemableLegacy {
+    function autoRedemption(
+        address _swapRouterAddress,
+        address _collateralAddr,
+        bytes memory _swapPath,
+        uint256 _amountIn
+    ) external returns (uint256 _amountOut);
+}
