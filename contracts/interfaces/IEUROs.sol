bd9ad62e510195aa55eac0cc6b43181554fc26c1
Ewan Sheldon <ewansheldon@gmail.com>
2025-01-28 18:13:34 +0100

    fmt

--- contracts/interfaces/IEUROs.sol
+++ contracts/interfaces/IEUROs.sol
@@ -4,5 +4,4 @@ pragma solidity 0.8.21;
 import "lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/access/IAccessControl.sol";
 import "lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-interface IEUROs is IERC20, IAccessControl {
-}
+interface IEUROs is IERC20, IAccessControl {}
