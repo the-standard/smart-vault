90eeff7abb12fb72d534370db8e7431214c4b2d9
Ewan Sheldon <ewansheldon@gmail.com>
2025-04-09 18:45:17 +0200

    change to euros liquidations

--- script/Liquidation.s.sol
+++ script/Liquidation.s.sol
@@ -6,6 +6,11 @@ import {IEUROs} from "../contracts/interfaces/IEUROs.sol";
 import {SmartVaultManagerV6} from "../contracts/SmartVaultManagerV6.sol";
 import {IRewardGateway} from "../contracts/interfaces/IRewardGateway.sol";
 
+interface ILegacyManager {
+    function liquidateVault(uint256 _tokenId) external;
+    function setLiquidatorAddress(address _liquidator) external;
+}
+
 contract Liquidation is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
@@ -13,12 +18,11 @@ contract Liquidation is Script {
         
         // EUROs
         // uint256 _tokenID;
-        // address manager = 0xba169cceCCF7aC51dA223e04654Cf16ef41A68CC;
-        // IRewardGateway gateway = IRewardGateway(0xb7ba62932d90C3c7ef841eF4339995eB93299dC9);
+        // ILegacyManager manager = ILegacyManager(0xba169cceCCF7aC51dA223e04654Cf16ef41A68CC);
         // IEUROs euros = IEUROs(0x643b34980E635719C15a2D4ce69571a258F940E9);
-        // euros.grantRole(bytes32(0), manager);
-        // gateway.liquidateVault(_tokenID);
-        // euros.revokeRole(bytes32(0), manager);
+        // euros.grantRole(bytes32(0), address(manager));
+        // manager.liquidateVault(_tokenID);
+        // euros.revokeRole(bytes32(0), address(manager));
 
         // USDs
         // uint256 _tokenID;
